159d9693ab48e18581eb8f7382910bebacea4ef1
ADriano AVelino <adriano-st777@hotmail.com>
2015-09-08 09:31:42 -0300

    adicionando fomrulario com tinymce e responsivefilmanager

--- tinymce/tinymce.php
+++ tinymce/tinymce.php
@@ -4,76 +4,130 @@
   <meta charset="utf-8">
   <title>JS Bin</title>
   <link rel="stylesheet" href="./../bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <script src="http://tinymce.cachefly.net/4.0/tinymce.min.js"></script>
-  <script src="http://localhost/github/labs/bower_components/jquery/dist/jquery.min.js" charset="utf-8"></script>
-  <script type="text/javascript">
-  tinymce.init({
-  selector: '#my_editor',
-  plugins: ["image"],
-  image_advtab: true,
-  file_browser_callback: teste,
-
-  // file_browser_callback: function(field_name, url, type, win) {
-  //     if(type=='image') $('#my_form input').click();
-  //     name = './images/'+ $("input").val().split("\\")[2];
-  //
-  //     // $("#" + field_name).val(name);
-  //     // console.log( win);
-  //
-  // },
+  <link rel="stylesheet" href="./../bower_components/select2/dist/css/select2.min.css">
+  <script src="./../bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="./../bower_components/tinymce/tinymce.min.js"></script>
+  <script src="./../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
 
-  //acessar iframe
-
-  images_upload_url: "postAcceptor.php"
-});
-
-
-function teste(field_name, url, type, win) {
-  var endereco = $("#mceu_49-body iframe").contents().find('#adriano').val();
-  $("#endereco").val(endereco);
-
-  tinymce.activeEditor.windowManager.open({
-    file: 'teste.html',// use an absolute path!
-    title: 'Upload de Arquivos',
-    width: 900,
-    height: 450,
-    resizable: 'yes'
-  }, {
-    setUrl: function (url) {
-      win.document.getElementById(field_name).value = url;
+  <style>
+    textarea {
+      min-height: 20em;
     }
-  });
-  return false;
-}
+  </style>
+  <script>
+    tinymce.init({
+      selector: "textarea",theme: "modern",
+      // height: 300,
+      // width: 700,height: 300,
+      language: "pt_BR",
+      plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen tabfocus",
+        "responsivefilemanager insertdatetime media table contextmenu paste wordcount"
+      ],
+      toolbar: "insertfile undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image table | code fullscreen  wordcount",
+      image_advtab: true ,
+      external_filemanager_path:"/github/labs/tinymce/filemanager/",
+      filemanager_title:"Gerenciador de Arquivos" ,
+      external_plugins: { "filemanager" : "/github/labs/tinymce/filemanager/plugin.min.js"}
+    });
 
-$("#t").click(function(){
-  alert('teste');
-});
+    $(function(){
 
+      $("#form select").select2({
+        language: "pt-BR",
+        placeholder: 'Selecione uma tag'
+      });
 
+      $("#form").on('submit',function(event){
+        event.preventDefault();
 
+        var titulo = $("#titulo").val();
+        var texto = tinyMCE.get('texto').getContent();
+        var tag = $("#tag").val();
 
-
+        $.post('enviar.php', {titulo:titulo, texto:texto, tag:tag}, function(data){
+          $("#box").html(data);
+        });
+      });
+    })
 
   </script>
 </head>
 <body>
-  <p></p>
-  <p></p>
-  <p></p>
-<input type="submit" name="" id="" class="btn">
-<p></p>
-<input type="text" name="endereco" value="endereco" id="endereco">
 
-<textarea id="my_editor"></textarea>
+  <div class="container">
 
-<iframe id="form_target" name="form_target" style="display:none"></iframe>
+    <div class="page-header" style="padding-left: 13em;">
+      <h1>Teste com tinymce<small> <br />
+      Um exemplo prático</small></h1>
+    </div>
 
-<form id="my_form" action="./postAcceptor.php" target="form_target" method="post" enctype="multipart/form-data" style="width:0px;height:0;overflow:hidden">
-  <input name="image" type="file" onchange="$('#my_form').submit();">
-</form>
+    <form class="form-horizontal" id="form">
+      <div class="form-group">
+        <label for="titulo" class="col-sm-2 control-label">Título: </label>
+        <div class="col-sm-10">
+          <input type="text" class="form-control" id="titulo" placeholder="Título">
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="texto" class="col-sm-2 control-label">Texto: </label>
+        <div class="col-sm-10">
+          <textarea class="form-control" id="texto"></textarea>
+        </div>
+      </div>
+      <div class="form-group">
+        <label for="tag" class="col-sm-2 col-md-2 col-xs-2 control-label">TAG: </label>
+        <div class="col-sm-8 col-md-9 col-xs-9">
+          <select class="form-control" name="" id="tag" placeholder="testeee" multiple>
+            <option value=""></option>
+            <option value="Rede">Rede</option>
+            <option value="Linux">Linux</option>
+            <option value="Windows Seven">Windows Seven</option>
+            <option value="VirtualBox">VirtualBox</option>
+            <option value="Manual">Manual</option>
+          </select>
+        </div>
+        <div class="sm-col-2">
+          <a href="#" class="pull-rigth" data-toggle="modal" data-target="#myModal">
+            <span class="glyphicon glyphicon-plus" style="font-size: 20px; top: 0.4em;" alt="Clique aqui para adicionar uma TAG" title="Clique aqui para adicionar uma TAG" data-toggle="popover" data-content="And here's some amazing content. It's very engaging. Right?"></span>
+          </a>
+        </div>
+      </div>
+      <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+          <button type="submit" class="btn btn-primary">Salvar</button>
+        </div>
+      </div>
+    </form>
+    <div id="box"></div>
+  </div>
+
+  <!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<button type="button" class="btn btn-default" data-container="body" data-toggle="popover" data-placement="left" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+  Popover on left
+</button>
 
 
+  <script src="./../bower_components/select2/dist/js/select2.min.js"></script>
+  <script src="./../bower_components/select2/dist/js/i18n/pt-BR.js"></script>
 </body>
 </html>
